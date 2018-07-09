cde765ede0fcb5929eab8de5be5d9fd4206a8791
tiagopariz <tiagopariz@gmail.com>
2018-07-09 11:34:13 -0300

    Correção

--- sql/CriarVendasDb.sql
+++ sql/CriarVendasDb.sql
@@ -15,33 +15,9 @@ CREATE TABLE [dbo].[Usuario]
 	);
 GO
 
-CREATE TABLE [dbo].[Produto]
-	(
-		[Id] INT IDENTITY NOT NULL PRIMARY KEY, 
-		[Descricao] VARCHAR(50) NOT NULL, 
-		[idUsuario] INT NULL
-	)
-GO
-
 INSERT 
 	INTO [dbo].[Usuario]([Usuario], [Senha])
 	VALUES ('tiagopariz', '123456'), 
 		   ('jonatanvies', '789012'), 
 		   ('alineloas', '345678');
-GO
-
-INSERT
-	INTO [dbo].[Produto]([Descricao], [idUsuario])
-	VALUES ('Pente', 1),
-		   ('Escova', 2),
-		   ('Bolinho', 2),
-		   ('Laranja', 3),
-		   ('Maçã', 1),
-		   ('Pepsi', 2),
-		   ('Fanta', 2),
-		   ('Tekitos', 3),
-		   ('Prato', 1),
-		   ('Sabão', 3),
-		   ('Mouse', 1),
-		   ('Hub', 3);
 GO
--- src/EvitandoAInjecaoDeSQL.Web/Controllers/ContaController.cs
+++ src/EvitandoAInjecaoDeSQL.Web/Controllers/ContaController.cs
@@ -16,7 +16,16 @@ namespace EvitandoAInjecaoDeSQL.Web.Controllers
         public ActionResult Login(string usuario, string senha)
         {
             var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=VendasDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            var consulta = "SELECT COUNT(*) FROM Usuario WHERE Usuario = '" + usuario + "' AND Senha = '" + senha + "'";
+            
+            // Código com a vunerabilidade devido a concatenação
+            //var consulta = "SELECT COUNT(*) FROM Usuario WHERE Usuario = '" + usuario + "' AND Senha = '" + senha + "'";
+
+            // Forma mais segura, montando o SELECT que espera os parâmetros @usuario e @senha
+            var consulta = "SELECT COUNT(*) " +
+                                "FROM Usuario " +
+                                "WHERE " +
+                                    "Usuario = @usuario AND " +
+                                    "Senha = @senha;";
 
             try
             {
@@ -26,6 +35,10 @@ namespace EvitandoAInjecaoDeSQL.Web.Controllers
 
                     using (SqlCommand comando = new SqlCommand(consulta, conexao))
                     {
+                        // Passando os parâmetros de SQL sem a necessidade de concatenar
+                        comando.Parameters.Add(new SqlParameter("@usuario", usuario));
+                        comando.Parameters.Add(new SqlParameter("@senha", senha));
+
                         var resultado = (int)comando.ExecuteScalar();
                         if (resultado > 0)
                             ViewBag.Mensagem = "Login efetuado com sucesso";
